6765c1b11e4a598378341a29e7aed4dd9bb3dda6
Craig Lucksted <luckstedcraig@gmail.com>
2018-09-24 11:56:11 -0500

    watch player health

--- app.js
+++ app.js
@@ -3,20 +3,62 @@ new Vue({
   data: {
     started: false,
     playerHealth: 100,
-    monsterHealth: 100
+    monsterHealth: 100,
+    attackMaxValue: 10,
+    healMaxValue: 5,
+    specialAttackMaxValue: 20,
+    turns: []
   },
   methods: {
-    attack: function () {
-
+    attack: function (range) {
+      var playerDamage = this.randomValue(range);
+      var monsterDamage = this.randomValue(range);
+      this.playerHealth -= playerDamage;
+      this.monsterHealth -= monsterDamage;
+      this.pushTurns([
+        playerDamage,
+        monsterDamage
+      ]);
     },
-    specialAttack: function () {
-
+    heal: function (range) {
+      this.playerHealth += this.randomValue(range);
+      if (this.playerHealth > 100) this.playerHealth = 100;
+      this.playerHealth -= this.randomValue(this.attackMaxValue);
+      this.pushTurns([
+        this.playerDamage,
+        this.monsterDamage
+      ]);
     },
-    heal: function () {
-
+    randomValue: function (range) {
+      return Math.floor(Math.random() * Math.floor(range));
+    },
+    startNew: function () {
+      this.started = true;
+      this.playerHealth = 100;
+      this.monsterHealth = 100;
+      this.turns = [];
+    },
+    pushTurns: function (turn) {
+      this.turns.push({
+        player: turn[0],
+        monster: turn[1]
+      });
     }
   },
-  computed: {
-
+  watch: {
+    playerHealth: function () {
+      if (this.playerHealth < 0 && this.monsterHealth > 0) {
+        alert('Player Lost');
+        this.startNew();
+      }
+      if (this.playerHealth > 0 && this.monsterHealth < 0) {
+        alert('Player Won');
+        this.startNew();
+      }
+      if (this.playerHealth < 0 && this.monsterHealth < 0) {
+        alert('Draw');
+        this.startNew();
+      }
+    }
   }
 });
